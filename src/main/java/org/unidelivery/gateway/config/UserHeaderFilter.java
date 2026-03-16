7374715a06206aa8ae53e989a171950343d90a6f
Achraf Sikal <achrafsikal@gmail.com>
2026-03-16 14:56:04 +0000

    feat: add UserHeaderFilter for adding user-id in the header of each request

---
+++ src/main/java/org/unidelivery/gateway/config/UserHeaderFilter.java
@@ -0,0 +1,43 @@
+package org.unidelivery.gateway.config;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.server.resource.authentication.AbstractOAuth2TokenAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class UserHeaderFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .cast(JwtAuthenticationToken.class)
+                .map(AbstractOAuth2TokenAuthenticationToken::getToken)
+                .flatMap(jwt -> {
+
+                    String userId = jwt.getSubject();
+
+                    ServerHttpRequest mutatedRequest = exchange.getRequest()
+                            .mutate()
+                            .header("X-User-Id", userId)
+                            .build();
+
+                    return chain.filter(exchange.mutate().request(mutatedRequest).build());
+                })
+                .switchIfEmpty(chain.filter(exchange));
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
