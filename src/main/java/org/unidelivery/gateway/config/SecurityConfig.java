9737dbc9b4060c7ccfdb341c532559f4f122b37b
Achraf Sikal <achrafsikal@gmail.com>
2026-03-23 17:11:38 +0100

    feat(gateway): update CORS configuration and modify API route paths for wallet and transaction services

--- src/main/java/org/unidelivery/gateway/config/SecurityConfig.java
+++ src/main/java/org/unidelivery/gateway/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:65487"));
-        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
